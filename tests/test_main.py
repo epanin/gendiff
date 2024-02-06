75d9e128986a2ebfb72bde47dbe74f4b477ba281
eugene <e.u.panin@yandex.ru>
2024-02-06 09:15:03 +0300

    UPDATE test_main, added using pytest.fixtures

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,13 @@
-from gendiff.main import generate_diff
+from gendiff import generate_diff
+import pytest
 
+@pytest.fixture
+def expected_result:
+    with open('tests/fixtures/expected_result.txt') as f:
+        return f.read()
 
-def test_generate_diff():
+def test_generate_diff_json(expected_result):
     result = generate_diff('tests/fixtures/file1.json', 
                            'tests/fixtures/file2.json')
-    awaiting = '''{
-    - follow: false
-    host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-    }'''
-    assert result == awaiting
+    assert result == expected_result
+
