bf6290ac042909f48bc353762b1ae4dae839dec2
Eugene <148694758+epanin@users.noreply.github.com>
2024-02-10 17:04:01 +0300

    FIX path-errors in test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from gendiff import generate_diff
 import pytest
+from pathlib import Path
 
 @pytest.fixture
 def expected_result():
@@ -7,11 +8,11 @@ def expected_result():
         return f.read()
 
 def test_generate_diff_json(expected_result):
-    result = generate_diff('tests/fixtures/file1.json', 
-                           'tests/fixtures/file2.json')
+    result = generate_diff(Path('tests/fixtures/file1.json'), 
+                           Path('tests/fixtures/file2.json'))
     assert result == expected_result
 
 def test_generate_diff_yaml(expected_result):
-    result = generate_diff('tests/fixtures/yaml1.yml',
-                           'tests/fixtures/yaml2.yml')
+    result = generate_diff(Path('tests/fixtures/yaml1.yml'),
+                           Path('tests/fixtures/yaml2.yml'))
     assert result == expected_result
