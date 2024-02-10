1945c298f1867dbba863adac54d86135b8ce2cb9
Eugene <148694758+epanin@users.noreply.github.com>
2024-02-10 16:57:52 +0300

    UPDATE main.py, copied func from finished exercise

--- gendiff/main.py
+++ gendiff/main.py
@@ -35,6 +35,8 @@ def generate_diff(file_path1, file_path2):
             list_of_changes.append(('    ', key, value_view))
             continue
         else: # element_changed
+
+
             list_of_changes.append(('  - ', key, value_view))
             new_value = sec_file.get(key)
             new_value_view = str(new_value).lower() if isinstance(new_value, bool) else str(new_value)
@@ -49,3 +51,20 @@ def transform_view(list_of_changes):
     for action, key, value in list_of_changes:
         answer = answer + action + str(key) + ': ' + str(value) + '\n'
     return answer + '}\n'
+
+def stringify(value, replacer=' ', spaces_count=1):
+
+    def iter_(current_value, depth):
+        if not isinstance(current_value, dict):
+            return str(current_value)
+
+        deep_indent_size = depth + spaces_count
+        deep_indent = replacer * deep_indent_size
+        current_indent = replacer * depth
+        lines = []
+        for key, val in current_value.items():
+            lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+        result = itertools.chain("{", lines, [current_indent + "}"])
+        return '\n'.join(result)
+
+    return iter_(value, 0)
