d6e6d12f86afa5e4236b3519f2ad79bd81f424c0
e.panin <fath34d@gmail.com>
2024-02-10 21:07:52 +0300

    FIX main.py linter errors

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,25 +1,25 @@
 from gendiff.utils import parse_files
+from itertools import chain
 
 
 def generate_diff(file_path1, file_path2):
     '''
     Compares two files as data structures. Supports json and yaml formats
 
-        Params:
-            file_path1 (PosixPath): the file to compare with
-            file_path2 (PosixPath): the file to be compared
+    Params:
+        file_path1 (PosixPath): the file to compare with
+           file_path2 (PosixPath): the file to be compared
 
-        Return:
-            result (str): the result of the comparison
+    Return:
+        result (str): the result of the comparison
     '''
     list_of_changes = []
     first_file, sec_file = parse_files(file_path1, file_path2)
     full_changes = sec_file.copy()
     full_changes.update(first_file)
     for key, value in full_changes.items():
-        
-        value_view = str(value).lower() if isinstance(value, bool) else str(value)
-        
+        value_view = (str(value).lower() if isinstance(value, bool)
+                      else str(value))
         element_deleted = key not in sec_file
         if element_deleted:
             list_of_changes.append(('  - ', key, value_view))
@@ -29,18 +29,17 @@ def generate_diff(file_path1, file_path2):
         if element_add:
             list_of_changes.append(('  + ', key, value_view))
             continue
-            
         element_same = value == sec_file.get(key)
         if element_same:
             list_of_changes.append(('    ', key, value_view))
             continue
-        else: # element_changed
-
-
+        else:  # element_changed
             list_of_changes.append(('  - ', key, value_view))
             new_value = sec_file.get(key)
-            new_value_view = str(new_value).lower() if isinstance(new_value, bool) else str(new_value)
-            list_of_changes.append(('  + ', key, new_value_view))        
+            new_value_view = (str(new_value).lower()
+                              if isinstance(new_value, bool)
+                              else str(new_value))
+            list_of_changes.append(('  + ', key, new_value_view))
 
     list_of_changes.sort(key=lambda x: x[1])
     return transform_view(list_of_changes)
@@ -51,6 +50,7 @@ def transform_view(list_of_changes):
     for action, key, value in list_of_changes:
         answer = answer + action + str(key) + ': ' + str(value) + '\n'
     return answer + '}\n'
+
 
 def stringify(value, replacer=' ', spaces_count=1):
 
@@ -64,7 +64,7 @@ def stringify(value, replacer=' ', spaces_count=1):
         lines = []
         for key, val in current_value.items():
             lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
-        result = itertools.chain("{", lines, [current_indent + "}"])
+        result = chain("{", lines, [current_indent + "}"])
         return '\n'.join(result)
 
     return iter_(value, 0)
